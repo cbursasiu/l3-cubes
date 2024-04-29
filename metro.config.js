e1199b3ef7bbbb155b8e38efc1a5f7ce9d1dbcca
Cristian Bursasiu <bursasiu@gmail.com>
2024-04-30 02:38:44 +0300

    added tree

--- metro.config.js
+++ metro.config.js
@@ -1,5 +1,5 @@
-const { getDefaultConfig } = require('expo/metro-config');
-const { mergeConfig } = require('@react-native/metro-config');
+const {getDefaultConfig} = require('expo/metro-config');
+const {mergeConfig} = require('@react-native/metro-config');
 
 /**
  * Metro configuration
@@ -7,6 +7,11 @@ const { mergeConfig } = require('@react-native/metro-config');
  *
  * @type {import('metro-config').MetroConfig}
  */
-const config = {};
+const config = {
+  resolver: {
+    sourceExts: ['js', 'jsx', 'json', 'ts', 'tsx', 'cjs', 'mjs'],
+    assetExts: ['glb', 'gltf', 'png', 'jpg'],
+  },
+};
 
 module.exports = mergeConfig(getDefaultConfig(__dirname), config);
