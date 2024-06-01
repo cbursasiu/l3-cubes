00e8d8451fce2700c9dfb6be6852562e8aa0e74b
Cristian Bursasiu <bursasiu@gmail.com>
2024-06-02 00:49:37 +0300

    fling

--- screens/MainScreen.tsx
+++ screens/MainScreen.tsx
@@ -1,18 +1,14 @@
 import {Canvas} from '@react-three/fiber';
 import React, {useRef} from 'react';
 
-import CubeModel from '../components/CubeModel';
-import {COLORS, COLUMNS_COUNT, ROWS_COUNT} from '../constants';
-import {RoundedBox, SpotLight} from '@react-three/drei';
+import {COLORS} from '../constants';
+import {RoundedBox} from '@react-three/drei';
 import {
-  Directions,
   Gesture,
   GestureDetector,
   GestureHandlerRootView,
 } from 'react-native-gesture-handler';
-import {Animated} from 'react-native';
 import {randInt} from 'three/src/math/MathUtils.js';
-import {useSprings} from '@react-spring/native';
 import {animated, useSpring} from '@react-spring/three';
 
 const MainScreen = () => {
@@ -20,59 +16,90 @@ const MainScreen = () => {
 
   const color = useRef(COLORS[randInt(0, colorsNumber - 1)]);
 
-  const [currentColor, api] = useSpring(() => ({
+  const [currentColor, colorApi] = useSpring(() => ({
     from: {color: color.current},
     config: {duration: 300},
   }));
 
-  const [rotation, apiRotation] = useSpring(() => ({
-    // from: {rotation: {x: 0, y: 0, z: 0}},
-    from: {rotation: {x: 0, y: 0, z: 0}},
-    config: {duration: 300},
-  }));
+  // const active = useSpringValue(false);
 
-  const moveGesture = Gesture.Pan().onEnd(() => {
-    color.current = COLORS[randInt(0, colorsNumber - 1)];
-    api.start({color: color.current});
-  });
+  const {scale} = useSpring({scale: 1});
+
+  // const {rotation} = useSpring({rotation: 0});
+
+  // const scRef = useRef(1);
+
+  const [springs, api] = useSpring(
+    () => ({
+      scale: 1,
+      position: [0, 0],
+      rotation: [0, 0, 0],
+    }),
+    [],
+  );
+
+  const rot = useRef([0, 0, 0]);
+
+  const flingStart = useRef({pos: [0, 0]});
+
+  const moveGesture = Gesture.Fling()
+    .onBegin(event => {
+      flingStart.current.pos = [event.x, event.y];
+    })
+    .onStart(event => {
+      const dx = event.x - flingStart.current.pos[0];
+      const dy = event.y - flingStart.current.pos[1];
+      if (Math.abs(dx) > Math.abs(dy)) {
+        if (dx > 0) {
+          rot.current[1] += (Math.PI / 180) * 90;
+        } else {
+          rot.current[1] -= (Math.PI / 180) * 90;
+        }
+      } else {
+        if (dy > 0) {
+          rot.current[0] += (Math.PI / 180) * 90;
+        } else {
+          rot.current[0] -= (Math.PI / 180) * 90;
+        }
+      }
+      // rot.current[0] += (Math.PI / 180) * 90;
+      api.start({rotation: rot.current});
+    })
+    .enabled(true)
+    .shouldCancelWhenOutside(false);
+
   return (
     <GestureHandlerRootView style={{flex: 1}}>
       <GestureDetector gesture={moveGesture}>
-        <Animated.View style={{flex: 1}}>
-          <Canvas
-            camera={{fov: 10, near: 0.1, far: 100, position: [0, 0, 100]}}>
-            <ambientLight intensity={0.4} color={'white'} />
-            <directionalLight color="white" position={[3, 3, 7]} />
-            {/* <SpotLight
-              distance={5}
-              angle={0}
-              attenuation={2}
-              anglePower={5} // Diffuse-cone anglePower (default: 5)
-            /> */}
-            <animated.mesh
-              scale={1}
-              key={1}
-              position={[0, 0, 5]}
-              // style={{rotation: rotation.rotation}}
-              rotation={rotation.rotation}
-              castShadow
-              onClick={() => {
-                console.log('click');
-                color.current = COLORS[randInt(0, colorsNumber - 1)];
-                api.start({color: color.current});
-              }}>
-              <RoundedBox
-                args={[1, 1, 1]} // Width, height, depth. Default is [1, 1, 1]
-                radius={0.1} // Radius of the rounded corners. Default is 0.05
-                smoothness={4} // The number of curve segments. Default is 4
-                bevelSegments={4} // The number of bevel segments. Default is 4, setting it to 0 removes the bevel, as a result the texture is applied to the whole geometry.
-                creaseAngle={0.1} // Smooth normals everywhere except faces that meet at an angle greater than the crease angle
-              >
-                <animated.meshPhongMaterial color={currentColor.color} />
-              </RoundedBox>
-            </animated.mesh>
-          </Canvas>
-        </Animated.View>
+        {/* <Animated.View style={{flex: 1}}> */}
+        <Canvas camera={{fov: 10, near: 0.1, far: 100, position: [0, 0, 100]}}>
+          <ambientLight intensity={0.4} color={'white'} />
+          <directionalLight color="white" position={[3, 3, 7]} />
+          <animated.mesh
+            scale={scale}
+            key={1}
+            position={[0, 0, 5]}
+            // position={position.pos}
+            rotation={springs.rotation.to((x, y, z) => [x, y, z])}
+            castShadow
+            // onClick={() => {
+            //   console.log('click');
+            // }}
+          >
+            <RoundedBox
+              // rotation={rotation.rotation}
+              scale={4}
+              args={[1, 1, 1]} // Width, height, depth. Default is [1, 1, 1]
+              radius={0.1} // Radius of the rounded corners. Default is 0.05
+              smoothness={4} // The number of curve segments. Default is 4
+              bevelSegments={4} // The number of bevel segments. Default is 4, setting it to 0 removes the bevel, as a result the texture is applied to the whole geometry.
+              creaseAngle={0.1} // Smooth normals everywhere except faces that meet at an angle greater than the crease angle
+            >
+              <animated.meshPhongMaterial color={currentColor.color} />
+            </RoundedBox>
+          </animated.mesh>
+        </Canvas>
+        {/* </Animated.View> */}
       </GestureDetector>
     </GestureHandlerRootView>
   );
